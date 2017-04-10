9031828535b83e57abfade9b68834eab8b199723
Ray <liujinjia@testerlife.com>
2017-04-11 07:37:37 +0800

    提交第二天课程信息及第一天的作业答案

---
+++ second_day/string_obj.py
@@ -0,0 +1,105 @@
+
+"""
+# -*- coding: utf-8 -*-
+# @Author: liujinjia
+# @Date:   2017-04-11 06:01:46
+# @FileName:  string_obj.py
+# @Project: Let-s-go-python-
+# @Last Modified by:   Ray
+# @Last Modified time: 2017-04-11 07:13:56
+"""
+
+# print(r'C:\some\name')   # C:\some\name    r''
+
+
+# print("""\
+# Usage: thingy [OPTIONS]\
+#      -h                        Display this usage message \
+#      -H hostname               Hostname to connect to\
+# """)
+
+# print("这是一本书" + "书名是鲁滨逊漂流")
+# print(3 * "Ray" + "mond")
+# print("abc""bcd")
+
+text = (
+    "这是一个测试信息，这条信息用来测试python"
+    "换行操作后的字符串，是否显示正常"
+)
+# print(text)
+# ['P', 'Y', 'T', 'H', 'O', 'N']
+#   0 ,  1 ,  2 ,  3 ,  4,   5
+#  -6   -5   -4   -3    -2   -1
+# word = 'Python'
+# print(word[0])      # P
+# print(word[2])      # t
+# print(word[5])      # n
+# print(word[-1])     # n
+# print(word[-3])     # h
+
+# print(word[2:5])
+# print(word[1:3])
+# print(word[2::])
+
+# print(word[:2] + word[2:])      # 包含起始的字符,不包含末尾的字符。这使得 s[:i] + s[i:] 永远等于 s
+name = "Raymond"
+# print(name[:4], name[4:])
+# print(name[:3], name[3:])
+# print(name[:2])
+# print(name[4:])
+# print(name[:3])
+
+# print(name[42:])   # 一个过大的索引值(即下标值大于字符串实际长 度)将被字符串实际长度所代替,当上边界比下边界大时(即切片左值大于右值)就返回空字符串
+
+ray = "Ray" + name[5:]
+# print(ray)
+# print(name)
+# print("Liu" + ray[:3])
+print('name\'s len: ', len(name))
+print('Ray\'s len', len(ray))
+print('string lenth', len('supercalifragilisticexpialidocious'))
+
+# len的长度取决于你当前使用的python编码
+
+print(len("中国"))
+
+# + %s 这种形式的字符串格式化 str1 + str2 = str3
+name = 'Ray'
+age = '18'
+text = "my name is {} , age is {}".format(name, age)
+# sql = "select * from user where user.mobile = {}".format(mobile)  # pymysql
+print(text)
+text = "my name is %s , age is %s" % ("Liu", "22")
+print(text)
+
+squares = [1, 4, 9, 16, 25]
+print(squares)
+print(squares[:])   # squares的浅拷贝信息
+
+print(squares + [36, 49, 64, 81, 100])
+
+cubes = [1, 8, 27, 65, 125]
+# 1 8 27 64 125
+print(cubes[3])     # 4 ** 3  is  64 not 65
+cubes[3] = 64
+print("修正后的list数据为：", cubes)
+cubes.append(216)
+cubes.append(7 ** 3)
+cubes.append(8 ** 3)
+cubes.insert(-1, 9 ** 3)
+print(cubes)
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+print(letters)
+letters[2:5] = ['C', 'D', 'E']
+print(letters)
+letters[2:5] = []
+print(letters)
+letters[:] = []
+print(letters)
+
+data = [[1, 2, 3], ['x', 'y', 'z'], ['a', 'b', 'c'], "a", "b"]
+print("len(data) = ", len(data))
+print(data[0][2])
+print(data[1][1])
+print(data[2][0])
---
+++ second_day/while.py
@@ -0,0 +1,68 @@
+
+"""
+# -*- coding: utf-8 -*-
+# @Author: liujinjia
+# @Date:   2017-04-11 07:02:49
+# @FileName:  while.py
+# @Project: Let-s-go-python-
+# @Last Modified by:   Ray
+# @Last Modified time: 2017-04-11 07:32:51
+"""
+# 菲波那契 子序列的程序
+
+a, b = 0, 1
+while b < 100:  # while 无限循环
+    print(b)
+    a, b = b, a + b
+print(a, b)
+
+# while 1 True
+# while []  False
+# while '' False
+
+a = 1  # False
+while a:
+    print("ok")
+    break
+
+
+# None  # nil null
+# bool  # True or False
+# set   一般我们对一个list进行去重并保留顺序的时候，可以使用set来进行操作
+
+# if result is None:
+#   do some thing
+
+_list_a = ['a', 'b', 'd', 'e']
+_list_b = ['d', 'a', 'c', 'f']
+
+_list_c = _list_a + _list_b
+print(set(_list_a) & set(_list_b))
+print(list(set(_list_c)))
+
+
+# 1 : print函数的作用是什么？
+# print函数会把参数信息打印在控制台
+
+# 2： python中常用到的数据类型判断的方法有哪些， 他们的区别是什么？
+# type, isinstance  区别是： type 返回的 是目标对象的类型， 而isinstance 返回的是bool
+
+# 3： 字典中取值方式有哪些，他们有什么不同
+# data = {'name': 'Ray', 'age': '18', 'sex': 'male'}
+# data.get('name1') , data['name1']
+# .get() 获取没有的key信息的时候， 返回None， data[]则抛出异常KeyError
+
+# 4： 我们如何快速的修改一个字典中指定key的value
+# dict.update(dict(key=value))
+
+# 5: json.dumps()只能针对于dict数据，还是可以操作其他类型？
+# json.dumps(list)
+
+# 6: 如何快速的获取字典中的全部元素
+# dict.items()
+
+# 7: 如何快速获取list全部元素的数量？
+# len(), list.__len__
+
+# 8: 如何快速的查看list中的全部方法
+# dir(list)
