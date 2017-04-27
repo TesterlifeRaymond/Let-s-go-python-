499d9d0b239fd7f4d22ba7107fb1082bc305a13c
Ray <liujinjia@testerlife.com>
2017-04-28 07:17:04 +0800

    modify some code

--- third_week/fifth_day/_ganji.py
+++ third_week/fifth_day/_ganji.py
@@ -6,7 +6,7 @@
 # @FileName:  _ganji.py
 # @Project: Let-s-go-python-
 # @Last Modified by:   Ray
-# @Last Modified time: 2017-04-28 07:06:26
+# @Last Modified time: 2017-04-28 07:16:07
 """
 import requests
 from bs4 import BeautifulSoup
@@ -25,12 +25,13 @@ class GanJi:
             page.encoding = 'utf-8'
         return BeautifulSoup(page.text, 'lxml')
 
-    def parse_page(self, url, tag=None, kw=None):
+    def get_page_all_url(self, url, tag=None, kw=None):
         """ pass """
         element = self.get_page_html(url).find_all(tag)
-        for item in element:
-            print(item.a)
+        return [
+            'http:/' + item.a.get('href') for item in element if item.a.get('href').startswith('/')
+        ]
 
 if __name__ == '__main__':
     ganji = GanJi()
-    ganji.parse_page('http://bj.ganji.com/', tag='s')
+    print(ganji.get_page_all_url('http://bj.ganji.com/', tag='s'))
