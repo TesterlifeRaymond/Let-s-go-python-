c7a2748f5a4839a822c4e7c92ea1a6d97c1f81bc
Ray <liujinjia@testerlife.com>
2017-05-07 07:06:18 +0800

    add seventh days files

--- __init__.py
+++ __init__.py
@@ -6,12 +6,5 @@
 # @FileName:  __init__.py
 # @Project: devops
 # @Last Modified by:   Ray
-# @Last Modified time: 2017-05-05 15:32:23
+# @Last Modified time: 2017-05-07 06:02:03
 """
-
-from warnings import warn
-warn(Warning('response iterable was set to a string.  This appears '
-             'to work but means that the server will send the '
-             'data to the client char, by char.  This is almost '
-             'never intended behavior, use response.data to assign '
-             'strings to the response object.'), stacklevel=2)
---
+++ fouth_week/seventh_day/encapsulation.py
@@ -0,0 +1,63 @@
+
+"""
+# -*- coding: utf-8 -*-
+# @Author: liujinjia
+# @Date:   2017-05-07 06:03:37
+# @FileName:  encapsulation.py
+# @Project: Let-s-go-python-
+# @Last Modified by:   Ray
+# @Last Modified time: 2017-05-07 07:05:08
+"""
+import gevent
+from requests import Session
+from lxml import etree
+
+
+class Base:
+    """ 父类 """
+    def __init__(self, encode=None):
+        """ init """
+        self.session = Session()
+        self.encode = encode
+
+    def request(self, url):
+        """ request active """
+        response = self.session.get(url)
+        response.encoding = self.encode
+        return response.text
+
+    def parse(self, url, path):
+        """ parse page element """
+        source = etree.HTML(self.request(url))
+        parse_html = source.xpath(path)
+        if parse_html:
+            return parse_html
+        return None
+
+
+class LaoShe(Base):
+    """ 继承Base类 """
+    def __init__(self, encode):
+        """ init class """
+        Base.__init__(self)
+        self.start_url = 'http://www.readers365.com/laoshewenji/lzdz/index.html'
+        self.base_url = 'http://www.readers365.com/laoshewenji/lzdz/'
+        self.testerhome_url = 'https://testerhome.com/topics/{}'
+
+    def get_all_urls(self):
+        """ pass """
+        return self.parse(self.start_url, '//a/@href')[4:-3]
+
+    def get_pages_text(self, url):
+        """ pass """
+        try:
+            result = self.parse(self.testerhome_url.format(url), '//h1/text()')[0]
+            print(''.join(result))
+        except Exception as ex:
+            print(ex)
+
+
+if __name__ == '__main__':
+    laoshe = LaoShe('utf-8')
+    events = [gevent.spawn(laoshe.get_pages_text, url) for url in range(1, 10000)]
+    gevent.joinall(events)
