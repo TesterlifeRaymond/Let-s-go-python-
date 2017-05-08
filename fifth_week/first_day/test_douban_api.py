44b90ced9d579b33a26f7b32ad5fe3961091a4f8
Ray <liujinjia@testerlife.com>
2017-05-08 10:26:57 +0800

    bugfix

--- fifth_week/first_day/test_douban_api.py
+++ fifth_week/first_day/test_douban_api.py
@@ -6,20 +6,25 @@
 # @FileName:  test_douban_api.py
 # @Project: Let-s-go-python-
 # @Last Modified by:   Ray
-# @Last Modified time: 2017-05-08 10:16:00
+# @Last Modified time: 2017-05-08 10:25:49
 """
+import unittest
+from requests import Session
 
-import requests
-from unittest import TestCase, main
 
-
-class DouBanApi(TestCase):
+class DouBanApi(unittest.TestCase):
     """ pass """
+    session = Session()
+    url = 'https://api.douban.com/v2/book/1220562'
 
     def test_douban_bookinfo(self):
         """ pass """
-        api_url = 'https://api.douban.com/v2/book/1220562'
-        print(requests.get(api_url, verify=False).json())
+        print('start test test_douban_api')
+        result = self.session.get(self.url, verify=False).json()
+        self.assertEqual(result.get('id'), '1220562')
+        self.assertEquals(result.get('title'), '满月之夜白鲸现')
+        self.assertEqual(type(result.get('tags')), list)
+        self.assertIn('片山恭一', result.get('author')[0], 'a in b')
 
 if __name__ == '__main__':
-    main()
+    unittest.main()
